eacd71180d9093a923138c13e7fa16074b4fb400
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-07-29 22:47:03 +0200

    Initial Commit

---
+++ Shared/Device_MockUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Device_MockUpApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 29/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Device_MockUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
